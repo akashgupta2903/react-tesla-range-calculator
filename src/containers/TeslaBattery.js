2f4ed1fae592bf4c6c639b6b02352d5dd035c7a7
akashgupta2903 <akash.gupta@gmail.com>
2018-04-06 01:35:45 -0700

    adding tesla car component

--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './TeslaBattery.css';
 import TeslaNotice from '../components/TeslaNotice/TeslaNotice'
+import TeslaCar from '../components/TeslaCar/TeslaCar'
 class TeslaBattery extends React.Component {
   render() {
     return (
       <form className="tesla-battery">
         <h1>Range Per Charge</h1>
+        <TeslaCar/>
         <TeslaNotice/>
       </form>
     )
