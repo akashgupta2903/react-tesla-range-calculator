e9b0b5848e2781d7c5edd6ff32da3d54e9eeb35d
akashgupta2903 <akash.gupta@gmail.com>
2018-04-06 01:22:34 -0700

    adding container component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import TeslaBattery from './containers/TeslaBattery'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <TeslaBattery/>
+      </div>
+    );
+  }
+}
+
+export default App;
